e4278af69cecbdc14d1da74d943d937707f232cc
ChokSheak Lau <choksheak@gmail.com>
2024-11-09 09:23:27 -0800

    Clean publish v0.1.5

---
+++ scripts/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd $(dirname "$0")/..
+
+echo CI
+npm run ci
+
+echo Build
+npm run build
+
+echo Generate types
+npm run gen-types
+
+echo Copy files
+cp -r package.json LICENSE README.md src dist
+
+echo Remove test files
+rm dist/*.test.* dist/src/*.test.*
+
+echo Publish
+cd dist
+npm publish --access=public
