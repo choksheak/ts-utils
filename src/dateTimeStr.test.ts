f0a36f433c42f56a3c8020eb86110e68814e1656
ChokSheak Lau <choksheak@gmail.com>
2025-01-19 16:08:16 -0800

    Remove DateTimeStr class

--- src/dateTimeStr.test.ts
+++ src/dateTimeStr.test.ts
@@ -1,5 +1,4 @@
-import { assert } from "./assert";
-import { DateTimeStr } from "./dateTimeStr";
+import { hhMmSs, hhMmSsMs, yyyyMmDd } from "./dateTimeStr";
 
 describe("DateTimeStr", () => {
   const dt1 = new Date(2024, 3, 6, 7, 9, 1, 4);
@@ -7,40 +6,28 @@ describe("DateTimeStr", () => {
   const dt3 = new Date(2024, 11, 31, 20, 21, 22, 234);
 
   test("yyyyMmDd", () => {
-    expect(DateTimeStr.yyyyMmDd(dt1)).toBe("2024-04-06");
-    expect(DateTimeStr.yyyyMmDd(dt2)).toBe("2024-11-12");
+    expect(yyyyMmDd(dt1)).toBe("2024-04-06");
+    expect(yyyyMmDd(dt2)).toBe("2024-11-12");
 
-    expect(DateTimeStr.yyyyMmDd(dt1, "/")).toBe("2024/04/06");
-    expect(DateTimeStr.yyyyMmDd(dt2, "+*")).toBe("2024+*11+*12");
+    expect(yyyyMmDd(dt1, "/")).toBe("2024/04/06");
+    expect(yyyyMmDd(dt2, "+*")).toBe("2024+*11+*12");
   });
 
   test("hhMmSs", () => {
-    expect(DateTimeStr.hhMmSs(dt1)).toBe("07:09:01");
-    expect(DateTimeStr.hhMmSs(dt2)).toBe("13:14:15");
+    expect(hhMmSs(dt1)).toBe("07:09:01");
+    expect(hhMmSs(dt2)).toBe("13:14:15");
 
-    expect(DateTimeStr.hhMmSs(dt1, "/")).toBe("07/09/01");
-    expect(DateTimeStr.hhMmSs(dt2, "+*")).toBe("13+*14+*15");
+    expect(hhMmSs(dt1, "/")).toBe("07/09/01");
+    expect(hhMmSs(dt2, "+*")).toBe("13+*14+*15");
   });
 
   test("hhMmSsMs", () => {
-    expect(DateTimeStr.hhMmSsMs(dt1)).toBe("07:09:01.004");
-    expect(DateTimeStr.hhMmSsMs(dt2)).toBe("13:14:15.016");
-    expect(DateTimeStr.hhMmSsMs(dt3)).toBe("20:21:22.234");
+    expect(hhMmSsMs(dt1)).toBe("07:09:01.004");
+    expect(hhMmSsMs(dt2)).toBe("13:14:15.016");
+    expect(hhMmSsMs(dt3)).toBe("20:21:22.234");
 
-    expect(DateTimeStr.hhMmSsMs(dt1, "/", "-")).toBe("07/09/01-004");
-    expect(DateTimeStr.hhMmSsMs(dt2, "+*", "^%")).toBe("13+*14+*15^%016");
-    expect(DateTimeStr.hhMmSsMs(dt3, "-", "#")).toBe("20-21-22#234");
-  });
-
-  test("local", () => {
-    // Skip timezone matching as it cannot be set in the test.
-    let t = DateTimeStr.local(dt1);
-    assert(t.startsWith("2024-04-06 07:09:01.004"), t);
-
-    t = DateTimeStr.local(dt2, "/", "@", ".", "+");
-    assert(t.startsWith("2024/11/12@13.14.15+016"), t);
-
-    t = DateTimeStr.local(dt3, ".", "t", "#", "%");
-    assert(t.startsWith("2024.12.31t20#21#22%234"), t);
+    expect(hhMmSsMs(dt1, "/", "-")).toBe("07/09/01-004");
+    expect(hhMmSsMs(dt2, "+*", "^%")).toBe("13+*14+*15^%016");
+    expect(hhMmSsMs(dt3, "-", "#")).toBe("20-21-22#234");
   });
 });
--- src/dateTimeStr.ts
+++ src/dateTimeStr.ts
@@ -1,83 +1,50 @@
-export class DateTimeStr {
-  public static yyyyMmDd(dt = new Date(), separator = "-"): string {
-    const yr = dt.getFullYear();
-    const mth = dt.getMonth() + 1;
-    const day = dt.getDate();
+export function yyyyMmDd(dt = new Date(), separator = "-"): string {
+  const yr = dt.getFullYear();
+  const mth = dt.getMonth() + 1;
+  const day = dt.getDate();
 
-    return (
-      yr +
-      separator +
-      (mth < 10 ? "0" + mth : mth) +
-      separator +
-      (day < 10 ? "0" + day : day)
-    );
+  return (
+    yr +
+    separator +
+    (mth < 10 ? "0" + mth : mth) +
+    separator +
+    (day < 10 ? "0" + day : day)
+  );
+}
+
+export function hhMmSs(dt = new Date(), separator = ":"): string {
+  const hr = dt.getHours();
+  const min = dt.getMinutes();
+  const sec = dt.getSeconds();
+
+  return (
+    (hr < 10 ? "0" + hr : hr) +
+    separator +
+    (min < 10 ? "0" + min : min) +
+    separator +
+    (sec < 10 ? "0" + sec : sec)
+  );
+}
+
+export function hhMmSsMs(
+  dt = new Date(),
+  timeSeparator = ":",
+  msSeparator = ".",
+): string {
+  const ms = dt.getMilliseconds();
+
+  return (
+    hhMmSs(dt, timeSeparator) +
+    msSeparator +
+    (ms < 10 ? "00" + ms : ms < 100 ? "0" + ms : ms)
+  );
+}
+
+export function tzShort(dt = new Date()): string {
+  if (dt.getTimezoneOffset() === 0) {
+    return "Z";
   }
 
-  public static hhMmSs(dt = new Date(), separator = ":"): string {
-    const hr = dt.getHours();
-    const min = dt.getMinutes();
-    const sec = dt.getSeconds();
-
-    return (
-      (hr < 10 ? "0" + hr : hr) +
-      separator +
-      (min < 10 ? "0" + min : min) +
-      separator +
-      (sec < 10 ? "0" + sec : sec)
-    );
-  }
-
-  public static hhMmSsMs(
-    dt = new Date(),
-    timeSeparator = ":",
-    msSeparator = ".",
-  ): string {
-    const ms = dt.getMilliseconds();
-
-    return (
-      DateTimeStr.hhMmSs(dt, timeSeparator) +
-      msSeparator +
-      (ms < 10 ? "00" + ms : ms < 100 ? "0" + ms : ms)
-    );
-  }
-
-  public static tz(dt = new Date()): string {
-    if (dt.getTimezoneOffset() === 0) {
-      return "Z";
-    }
-
-    const tzHours = dt.getTimezoneOffset() / 60;
-    return tzHours >= 0 ? "+" + tzHours : String(tzHours);
-  }
-
-  /** Full local date/time string. */
-  public static local(
-    dt = new Date(),
-    dateSeparator = "-",
-    dtSeparator = " ",
-    timeSeparator = ":",
-    msSeparator = ".",
-  ): string {
-    return (
-      DateTimeStr.yyyyMmDd(dt, dateSeparator) +
-      dtSeparator +
-      DateTimeStr.hhMmSsMs(dt, timeSeparator, msSeparator) +
-      DateTimeStr.tz(dt)
-    );
-  }
-
-  /** Use the default ISO string function to keep things simple here. */
-  public static utc(dt = new Date()) {
-    return dt.toISOString();
-  }
-
-  /** Default full local date+time string with full & concise info. */
-  public static get now() {
-    return DateTimeStr.local();
-  }
-
-  /** Default full UTC date+time string with full & concise info. */
-  public static get utcNow() {
-    return DateTimeStr.utc();
-  }
+  const tzHours = dt.getTimezoneOffset() / 60;
+  return tzHours >= 0 ? "+" + tzHours : String(tzHours);
 }
