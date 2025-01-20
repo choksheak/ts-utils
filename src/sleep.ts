9228a3b46d1477696beeaf23b28f482d1f312487
ChokSheak Lau <choksheak@gmail.com>
2025-01-20 05:04:44 -0800

    Add comments for sleep

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,3 +1,7 @@
+/**
+ * Sleep for a given number of milliseconds. Note that this method is async,
+ * so please remember to call it with await, like `await sleep(1000);`.
+ */
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
