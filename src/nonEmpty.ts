8722130ae3ca728d0c3bf4e74c5d6b3cd4cb453f
ChokSheak Lau <choksheak@gmail.com>
2025-01-20 05:00:36 -0800

    Add comments for varName param

--- src/nonEmpty.ts
+++ src/nonEmpty.ts
@@ -3,6 +3,9 @@ import { isEmpty } from "./isEmpty";
 /**
  * Type asserts that `t` is truthy.
  * Throws an error if `t` is null or undefined.
+ *
+ * @param varName The variable name to include in the error to throw when t is
+ *   empty. Defaults to 'value'.
  */
 export function nonEmpty<T>(
   t: T | null | undefined | "" | 0 | -0 | 0n | false | typeof NaN,
--- src/nonNil.ts
+++ src/nonNil.ts
@@ -1,6 +1,9 @@
 /**
  * Type asserts that `t` is neither null nor undefined.
  * Throws an error if `t` is null or undefined.
+ *
+ * @param varName The variable name to include in the error to throw when t is
+ *   nil. Defaults to 'value'.
  */
 export function nonNil<T>(t: T | null | undefined, varName = "value"): T {
   if (t === null || t === undefined) {
