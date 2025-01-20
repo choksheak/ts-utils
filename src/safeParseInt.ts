240dd7c748b76fbdb1f3a7e824d32f6453dde32a
ChokSheak Lau <choksheak@gmail.com>
2025-01-20 05:03:09 -0800

    Add comments for safeParseInt

--- src/safeParseInt.ts
+++ src/safeParseInt.ts
@@ -1,5 +1,8 @@
 /**
  * Returns 0 if the string is not a valid number.
+ *
+ * @param logError Log a console error if the given string is not a valid
+ *   number. Defaults to false (don't log anything).
  */
 export function safeParseInt(s: string, logError = false): number {
   const i = Number(s);
