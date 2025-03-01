14c563917bb2e99ab13ca24cb0c4e3c90548cf42
ChokSheak Lau <choksheak@gmail.com>
2025-03-01 11:49:59 -0800

    Add safeParseFloat

--- src/safeParseFloat.ts
+++ src/safeParseFloat.ts
@@ -1,10 +1,10 @@
 /**
  * Returns 0 or the given defaultValue if the string is not a valid number.
  */
-export function safeParseInt<T>(
+export function safeParseFloat<T>(
   s: string,
   defaultValue: T | number = 0,
 ): T | number {
   const i = Number(s);
-  return isNaN(i) ? defaultValue : Math.floor(i);
+  return isNaN(i) ? defaultValue : i;
 }
