e38dfcff3dcf6859e415658ff4e93e8bc46ba6ff
ChokSheak Lau <choksheak@gmail.com>
2025-11-27 21:06:02 -0800

    Slight improve asNumber

--- src/asNumber.ts
+++ src/asNumber.ts
@@ -2,15 +2,17 @@
  * Coerce `u` into a number if possible, otherwise just return 0.
  */
 export function asNumber(u: unknown): number {
-  // No transformation needed if u is already a number.
-  if (typeof u === "number" && isFinite(u)) {
-    return u;
+  // If u is a valid number, return it.
+  if (typeof u === "number") {
+    return isFinite(u) ? u : 0;
   }
 
-  // Try to make into a number if possible.
-  const n = Number(u);
-  if (typeof n === "number" && isFinite(n)) {
-    return n;
+  // Try to make into a number if not already a number.
+  u = Number(u);
+
+  // If u is a valid number, return it.
+  if (typeof u === "number" && isFinite(u)) {
+    return u;
   }
 
   // Return 0 for everything else. This is usually ok if want to just ignore
