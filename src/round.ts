18a7fc0c6ed3d9722333a90f29c8da6b800ed431
ChokSheak Lau <choksheak@gmail.com>
2025-11-27 20:55:06 -0800

    Nit update doc for round()

--- src/round.ts
+++ src/round.ts
@@ -2,7 +2,7 @@
  * Simplifies rounding of floating point numbers. Note that if your number ends
  * with zeros, and you convert the number to string, you will lose the zeroes
  * at the end. To show the exact number of decimal places, you'll need to use
- * toFixed(). E.g. round(1.20, 2).toFixed(2).
+ * roundToString() or toFixed(). E.g. round(1.20, 2).toFixed(2).
  */
 export function round(n: number, numDecimalPlaces = 0): number {
   const multipler = Math.pow(10, numDecimalPlaces);
