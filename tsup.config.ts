ce41b3a8e7a6ec2cb42d6a224464045c069e0786
ChokSheak Lau <choksheak@gmail.com>
2025-12-27 17:37:19 -0800

    Prevent bundling duplicate code

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig([
   {
     // Non-minified development build
-    entry: ["src/index.ts"],
     clean: true,
     format: ["cjs", "esm"],
     dts: true,
-    splitting: false,
+    splitting: true,
     external: ["react", "react-dom"],
     outExtension: ({ format }) => ({
       js: format === "cjs" ? ".cjs" : ".mjs",
@@ -15,12 +14,11 @@ export default defineConfig([
   },
   // Minified production build
   {
-    entry: ["src/index.ts"],
     format: ["cjs", "esm"],
     dts: false, // No need for duplicate declarations
     sourcemap: true,
     minify: true,
-    splitting: false,
+    splitting: true,
     external: ["react", "react-dom"],
     outExtension: ({ format }) => ({
       js: format === "cjs" ? ".min.cjs" : ".min.mjs",
